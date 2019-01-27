cd183374ade9f112f7d5e3834dcbd66aaeb3c071
NickNish09 <nnmarques97@gmail.com>
2019-01-27 00:11:28 -0200

    rodou template no rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(vendor/bootstrap/css/bootstrap.min.css)
+Rails.application.config.assets.precompile += %w(vendor/fontawesome-free/css/all.min.css)
+Rails.application.config.assets.precompile += %w(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic)
+Rails.application.config.assets.precompile += %w(vendor/simple-line-icons/css/simple-line-icons.css)
+Rails.application.config.assets.precompile += %w(css/stylish-portfolio.css)
+Rails.application.config.assets.precompile += %w(vendor/jquery/jquery.min.js)
+Rails.application.config.assets.precompile += %w(vendor/bootstrap/js/bootstrap.bundle.min.js)
+Rails.application.config.assets.precompile += %w(vendor/jquery-easing/jquery.easing.min.js)
+Rails.application.config.assets.precompile += %w(js/stylish-portfolio.min.js)
+Rails.application.config.assets.precompile += %w(*.jpg *.png)
