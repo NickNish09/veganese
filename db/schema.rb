269ef940888309a89937d4a314f5960239644850
NickNish09 <nnmarques97@gmail.com>
2019-01-26 22:44:56 -0200

    boostrap 4 instalado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_001247) do
+ActiveRecord::Schema.define(version: 2019_01_27_001650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_01_27_001247) do
     t.string "ingredient_5"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.string "name"
   end
 
 end
